49e043507f8441c4e3efcd645290d02bc5e4f3e2
RoddikVilla <roddik.villa@epn.edu.ec>
2026-01-10 19:03:35 -0500

    Update Punto 1y2

--- Declaracion de Funciones.cpp
+++ Declaracion de Funciones.cpp
@@ -4,25 +4,61 @@
 //-Sanchez Sanchez Joel Efrain
 //Tema:
 //Sistema CRUD de gestión y clasificación de música
-
-
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <sstream>
 using namespace std;
+struct cancion{
+	string nombre,genero,artista;
+	int id;
+	double duracion;
+};
 
-void mostrarRegistros(){
+void IngresarCancion(vector<cancion> &s){
+	cancion song;
+	ofstream archivo("music.txt", ios::app);
+	
+	cout<<"Ingrese ID de la cancion: ";
+	cin>>song.id;
+	cin.ignore();
+	cout<<"Ingrese el nombre de la cancion: ";
+	getline(cin,song.nombre);
+	cout<<"Ingrese el artista: ";
+	getline(cin,song.genero);
+	cout<<"Ingrese el genero: ";
+	getline(cin,song.artista);
+	cout<<"Ingrese la duracion: ";
+	cin>>song.duracion;
+	
+	s.push_back(song);
+	
+	cout<<"-Cancion Registrada con Exito-"<<endl;
+}
+
+void mostrarRegistro(vector<cancion> s){
+	cout<<"\n-Registro de canciones-"<<endl;
+	
+	for(auto song : s){
+		cout<<"\nID: "<<song.id<<endl;
+		cout<<"Nombre: "<<song.nombre<<endl;
+		cout<<"Artista: "<<song.artista<<endl;
+		cout<<"Genero: "<<song.genero<<endl;
+		cout<<"Duracion: "<<song.duracion<<endl;
+	}
+	
 	
 }
-void IngresarCancion(){
+//void editar(){
+//	
+//}
 
-}
-
-void editar
-
-void menuPrincipal(int opcion){
+void menuPrincipal(vector<cancion> &s){
+	
+	int opcion;
 	do{
-		cout<<"===GESTION Y CLASIFICACION DE MUSICA==="<<endl;
+		cout<<"\n===GESTION Y CLASIFICACION DE MUSICA==="<<endl;
 		cout<<"1.Ingresar una Cancion: "<<endl;
 		cout<<"2.Mostrar Registro de Canciones"<<endl;
 		cout<<"3.Editar Registro de Canciones"<<endl;		
@@ -34,8 +70,10 @@ void menuPrincipal(int opcion){
 		
 		switch(opcion){
 			case 1:
+				IngresarCancion(s);
 				break;
 			case 2:
+				mostrarRegistro(s);
 				break;
 			case 3:
 				break;
@@ -44,7 +82,7 @@ void menuPrincipal(int opcion){
 			case 5:
 				break;
 			case 0:
-				cout<<"Saliendo..."<<endl;
+				cout<<"Saliendo del Sistema...\n-Que tenga un Lindo Dia-"<<endl;
 				break;	
 		}
 	}while(opcion != 0);
@@ -54,10 +92,9 @@ void menuPrincipal(int opcion){
 
 int main(){
 	int opc;
-	
-	menuPrincipal(opc);
+	vector<cancion> canciones; 
+	menuPrincipal(canciones);
 	
 	
 	return 0;
 }
-
